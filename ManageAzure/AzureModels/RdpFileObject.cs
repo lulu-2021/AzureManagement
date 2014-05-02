ee51d26109cb7c953003c1b9241ea542dd8a6d6e
Steve Forkin <steve@janison.com.au>
2014-05-02 17:49:41 +1000

    added functions to download all the RDP files for all types of cloud services i.e. vm or elastic

---
+++ ManageAzure/AzureModels/RdpFileObject.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.WindowsAzure.Management.Compute.Models;
+using System;
+
+namespace ManageAzure.AzureModels
+{
+    public class RdpFileObject
+    {
+        public string RdpFileName { get; set; }
+        public VirtualMachineGetRemoteDesktopFileResponse RdpObject { get; set; }
+
+        public RdpFileObject(string rdpFileName, VirtualMachineGetRemoteDesktopFileResponse rdpObject)
+        {
+            RdpFileName = rdpFileName;
+            RdpObject = rdpObject;
+        }
+    }
+}
